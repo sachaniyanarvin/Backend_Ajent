51b2c15699db316c4392fe45bdb091be5614f906
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2025-01-31 13:22:50 +0530

    Error handing problem solve it

--- Backend/App.js
+++ Backend/App.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import morgan from 'morgan';
 import connect from './DataBase/db.js';
-import userRoutes from './routes/user.routes.js';
-import projectRoutes from './routes/project.routes.js';
-import aiRoutes from './routes/ai.routes.js';
+import userRoutes from './Routes/user.routes.js';
+import projectRoutes from './Routes/project.routes.js';
+import aiRoutes from './Routes/ai.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
 connect();
 
+const app = express();
 
-const app = express();  
-
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,12 +22,16 @@ app.use(cookieParser());
 
 app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
-app.use("/ai", aiRoutes)
-
-
+app.use('/ai', aiRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-export default app; 
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error('Global error:', err);
+    res.status(500).json({ error: 'Internal server error' });
+});
+
+export default app;
